836a7957984e775935b26a5f4e9a0ca29cfb53b5
cardenalcopia <kirisse85@gmail.com>
2022-05-14 10:05:39 +0200

    Subida repaso 3

--- OOPRewiev/repaso3.ts
+++ OOPRewiev/repaso3.ts
@@ -1,5 +1,5 @@
 import { Mobile } from "./repaso2";
-class MobileLibrary 
+export class MobileLibrary 
 {
     constructor(private name:string,private location:string,private mobiles : Mobile[],private totalPrice:number)
 
@@ -9,6 +9,7 @@ class MobileLibrary
         this.name = name ;
         this.location=location;
         this.mobiles=mobiles;
+        this.totalPrice= this.totalPriceCalculation();
 
     }
     public getName(): string {
@@ -33,7 +34,7 @@ class MobileLibrary
         this.totalPrice = totalPrice;
     }
 
-    public totalPriceCalculation ()
+    private totalPriceCalculation ():number
     {let contador : number =0;
 
         for (let i=0; i<this.mobiles.length;i++)
@@ -42,11 +43,24 @@ class MobileLibrary
         
         }
         console.log ("El precio total de la biblioteca es :"+ contador+"€");
+        return contador;
     }
+    public printLibrary (a:Mobile[])
+    {
+        let contador : number =0;
+        console.log ("This is all my mobiles : \n");
+        for (let i=0;i<this.mobiles.length;i++)
+        {
+            console.log("The characteristics of the mobile "+this.mobiles[i].getName()+" are: \n"+"Name : "+this.mobiles[i].getName() +"\n"+"Model : "+this.mobiles[i].getModel()+"\n"+"Trademark : "+this.mobiles[i].getTrademarck() +"\n"+ "SD size(GB) : "+this.mobiles[i].getSdSize()+"\n"+"Color : "+this.mobiles[i].getColor() +"\n"+"Is 5g? : "+this.mobiles[i].isIs5G()+"\n"+"Number of Cameras : "+this.mobiles[i].getcameraNumber() )
+           
+        }
+        console.log ("Price overall :"+this.totalPrice);
+    }
+
 }
 let nokia= new Mobile("Nokia3210","3210","Nokia",64,"azul",false,0,10);
 let iPhone3g =new Mobile("iPhone","3G","Apple",256,"Blanco",false,1,150);
 let samsung= new Mobile("Galaxy 10","10","Samsung",512,"Negro",true,3,200);
 let miArray:Mobile[]=([nokia,iPhone3g,samsung]);
 let libreria = new MobileLibrary("Libreria 1","Madrid",miArray,0);
-libreria.totalPriceCalculation();
+//libreria.totalPriceCalculation();
--- OOPRewiev/repaso3Test.ts
+++ OOPRewiev/repaso3Test.ts
@@ -3,5 +3,7 @@ import { MobileLibrary } from "./repaso3";
 let nokia= new Mobile("Nokia3210","3210","Nokia",64,"azul",false,0,10);
 let iPhone3g =new Mobile("iPhone","3G","Apple",256,"Blanco",false,1,150);
 let samsung= new Mobile("Galaxy 10","10","Samsung",512,"Negro",true,3,200);
-let xiaomi= new Mobile("Xiaomi","Mi Note 10","Xiaomi",512,"Blanco",true,4,300);
+let xiaomi= new Mobile("Xiaomi","Mi Note 10","Xiaomi",512,"Blanco",true,4,310);
 let miArray:Mobile[]=([nokia,iPhone3g,samsung,xiaomi]);
+let milibreria = new MobileLibrary("Libreria 2","Cadiz",miArray,0);
+milibreria.printLibrary(miArray);
